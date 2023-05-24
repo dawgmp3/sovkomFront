cf90fd84718bb834c19589fbf3a0ce6f27ea4f84
kopytyoshka <ilyatttt19@gmail.com>
2023-05-24 23:20:01 +0300

    add: HR Main Page, Candidate Main Page, PopUp-Menu

---
+++ src/pages/Popup-Menu-Candidate.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import '../styles/Popup-Menu-Style.css'
+import {
+    IonBadge,
+    IonButtons,
+    IonContent,
+    IonHeader, IonIcon,
+    IonItem, IonLabel, IonList,
+    IonMenu,
+    IonMenuButton, IonMenuToggle,
+    IonPage,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
+
+function PopupMenuCandidate() {
+    return (
+        <>
+            <IonMenu contentId="main-content">
+                <IonHeader>
+                    <IonToolbar>
+                        <IonTitle>Платформа</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent className="ion-no-padding">
+                    <IonList>
+                        <IonMenuToggle>
+                            <IonItem routerLink="/">
+                                <IonIcon src="./images/pulse-outline.svg" slot="start"></IonIcon>
+                                <IonLabel>Главная страничка</IonLabel>
+                            </IonItem>
+                        </IonMenuToggle>
+                        <IonMenuToggle>
+                            <IonItem routerLink="/">
+                                <IonIcon src="./images/ice-cream-outline.svg" slot="start"></IonIcon>
+                                <IonLabel>Все вакансии</IonLabel>
+                            </IonItem>
+                        </IonMenuToggle>
+                        <IonMenuToggle>
+                            <IonItem routerLink="/">
+                                <IonIcon src="./images/calendar-number-outline.svg" slot="start"></IonIcon>
+                                <IonLabel>Мои задания</IonLabel>
+                            </IonItem>
+                        </IonMenuToggle>
+                        <IonItem className="ion-padding-top">
+                            <IonLabel className="ion-padding-top">Дополнительно</IonLabel>
+                        </IonItem>
+                        <IonMenuToggle>
+                            <IonItem routerLink="/">
+                                <IonIcon src="./images/terminal-outline.svg" slot="start"></IonIcon>
+                                <IonLabel>Статистика</IonLabel>
+                            </IonItem>
+                        </IonMenuToggle>
+                    </IonList>
+                </IonContent>
+            </IonMenu>
+            <IonPage id="main-content">
+                <IonHeader>
+                    <IonToolbar>
+                        <IonButtons slot="start">
+                            <IonMenuButton></IonMenuButton>
+                        </IonButtons>
+                        <IonTitle>Menu</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                {/*<IonContent className="ion-padding">тут отрисовка говна</IonContent>*/}
+            </IonPage>
+        </>
+    );
+}
+
+export default PopupMenuCandidate;
